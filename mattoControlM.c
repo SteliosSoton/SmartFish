c49ef080ec5016ddb7d7c58a206b062b483bfa72
mp7g16 <mattpricesemail@gmail.com>
2018-02-20 14:53:38 +0000

    fixed SPI with SS toggle

--- mattoControlM.c
+++ mattoControlM.c
@@ -9,49 +9,53 @@
 #include <util/delay.h>
 #include "debug.h"
 
-void init_spi_master(void) {
-	/* out: MOSI, SCK, /SS, in: MISO */
-	DDRB = _BV(PB4) | _BV(PB5) | _BV(PB7); // ?: PB4 (SS), PB5 (MOSI), PB7(SCK)
+char data;
+char toSend[10] = "abcdefghij";
 
-	SPCR = _BV(SPE) | _BV(MSTR) | _BV(SPR0) | _BV(SPR1);	// SPI Control Register
+
+void init_spi_master(void) {
+	// out: MOSI, SCK, SS (in: MISO)
+	DDRB = _BV(PB4) | _BV(PB5) | _BV(PB7); // Set = 1: PB4 (SS), PB5 (MOSI), PB7(SCK)
+
+	PORTB = 0xFF; //This is very important!! need to toggle Slave select line from
+
+	SPCR = (1 << SPE) | (1 << MSTR) | (0 << SPI2X) | (1 << SPR0) | (1 << SPR1);	// SPI Control Register
 	// SPE - the SPI is enabled when this bit is 1
 	// MSTR - configures SPI as master
 	// SPI2X, SPR0, SPR1 - configure SPI clock frequency (0 1 1 fosc/128)
-}
 
-void tx(uint8_t b) {
-	SPDR = b;	// configure SPI Data Register
+	PORTB &= ~(1 << PB4); //toggle slave select line from high to low
+
+}
+		//uint8_t
+void tx(char sendData) {
+	SPDR = sendData;	// configure SPI Data Register
 	while(!(SPSR & _BV(SPIF)));	// wait for transmission complete
-}
-
-void init_spi_slave(void) {
-	/* out: MISO, in: MOSI, SCK, /SS */
-	DDRB = _BV(PB6);	// Pull-up resistor
-	SPCR = _BV(SPE);	/* Enable SPI */
-}
-
-uint8_t rx(void) {
-	while(!(SPSR & _BV(SPIF)));	// wait for transmission complete
-	return SPDR;
 }
 
 int main(void)
 {
 	init_spi_master();	// initialise SPI as master or slave
-	init_debug_uart0();
-    // init_spi_slave();
-
-    // while(1) {
-        // printf("%d", rx(););
-    // }
-
-	int i=0;
+	init_debug_uart0();	// initialise UART debug
 
 	while(1){
-		tx(i);
-        printf("\n%d", i);
 
-		if(i < 254) i++;
-		else i =0;
+		for(int j=0;j<sizeof(toSend);j++){
+			tx(toSend[j]);
+			printf("\n%c", toSend[j]);
+			_delay_ms(5);
+		}
+
+
+		/*
+		for(int i=0;i<254;i++)
+		{
+			tx(i);
+			printf("\n%d", i);
+			_delay_ms(10);
+		}
+		*/
+
 	}
+
 }
