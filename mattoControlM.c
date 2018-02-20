0f68910c7860bf48fffaa1ad051e63083b1a622e
mp7g16 <mattpricesemail@gmail.com>
2018-02-20 14:03:06 +0000

    adding seperate Master File

--- mattoControlM.c
+++ mattoControlM.c
@@ -13,20 +13,10 @@ void init_spi_master(void) {
 	/* out: MOSI, SCK, /SS, in: MISO */
 	DDRB = _BV(PB4) | _BV(PB5) | _BV(PB7); // ?: PB4 (SS), PB5 (MOSI), PB7(SCK)
 
-	SPCR = _BV(SPE) | _BV(MSTR) | _BV(SPI2X);	// SPI Control Register
+	SPCR = _BV(SPE) | _BV(MSTR) | _BV(SPR0) | _BV(SPR1);	// SPI Control Register
 	// SPE - the SPI is enabled when this bit is 1
 	// MSTR - configures SPI as master
-	// SPI2X - double SPI Speed bit, SPI speed (SCK Frequency) doubles in Master Mode
-	/* F_SCK = F_CPU/2 */
-}
-
-void init_spi_slave(void) { /* out: MISO, in: MOSI, SCK, /SS */
-    DDRB = _BV(PB6); SPCR = _BV(SPE); /* Enable SPI */ 
-}
-
-uint8_t rx(void) {
-    while(!(SPSR & _BV(SPIF))); 
-    return SPDR; 
+	// SPI2X, SPR0, SPR1 - configure SPI clock frequency (0 1 1 fosc/128)
 }
 
 void tx(uint8_t b) {
@@ -48,14 +38,20 @@ uint8_t rx(void) {
 int main(void)
 {
 	init_spi_master();	// initialise SPI as master or slave
+	init_debug_uart0();
     // init_spi_slave();
-    
+
     // while(1) {
         // printf("%d", rx(););
     // }
-    
+
+	int i=0;
+
 	while(1){
-		tx(150);
-		_delay_ms(500);
+		tx(i);
+        printf("\n%d", i);
+
+		if(i < 254) i++;
+		else i =0;
 	}
 }
