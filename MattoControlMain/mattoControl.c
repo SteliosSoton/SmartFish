c6a12bcf3bd7f07bb14bfac61cad1bda6856f9ad
Nathan <nrs1g15@soton.ac.uk>
2018-02-25 15:04:42 +0000

    Added INT0 code

--- MattoControlMain/mattoControl.c
+++ MattoControlMain/mattoControl.c
@@ -7,9 +7,15 @@
 #include "debug.h"
 #include "power_saving.h"
 #include "SPI_slave.h"
+#include "external_interrupt.h"
 
 int main(void)
 {
+	/*
+	 * Removed inits when they are not needed for certain testing.
+	 * E.G when when using external interrupt for testing ADC comment out SPI init.
+	 */
+
 	init_debug_uart0();
     printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 	printf("\nUART debug init complete");
@@ -19,6 +25,8 @@ int main(void)
     printf("\nPower saving complete:\n\ttimer0 disabled \n\ttimer1 disabled \n\ttwi disabled \n\tusart1 disabled");
     init_spi_slave();
     printf("\nSPI slave enabled");
+    init_external_interrupts();
+    printf("\nInt0 enabled for ADC conversions testing");
     _delay_ms(1); //Device enters sleep too fast for UART to send data 
     
 	while(1) { enterSleepMode(IDLE); //Keep device in IDLE while waiting for SPI transfer complete interrupt
