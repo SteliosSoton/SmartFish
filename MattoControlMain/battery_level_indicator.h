04c22b5081bac33dfba17339b551742e936ce83d
Nathan <nrs1g15@soton.ac.uk>
2018-02-26 17:02:04 +0000

    Added conversion to percentage for battery level

--- MattoControlMain/battery_level_indicator.h
+++ MattoControlMain/battery_level_indicator.h
@@ -12,7 +12,11 @@ double getBatteryLevel(uint16_t ADCData)
 {
 	double ADCV = (ADCData*3.3)/1024;
 	double dividerNetwork = 120217.0 /(389735 + 120217);
-	return ADCV/dividerNetwork; //Convert ADC hex to voltage then potential divider network of measured resistances
+    double percentage = 18.182 * (ADCV/dividerNetwork) - 118.18;
+    if(percentage > 100)
+    	return 100;
+    else
+    	return percentage; //Convert ADC hex to voltage then potential divider network of measured resistances
 }
 
 #endif /* BATTERY_LEVEL_INDICATOR_H_ */
