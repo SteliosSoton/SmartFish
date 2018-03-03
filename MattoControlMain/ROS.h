9c5a861a766be77b4d2225632e6a8c8d189af0ed
SteliosSoton <36567553+SteliosSoton@users.noreply.github.com>
2018-03-03 19:43:07 +0000

    Look more like proper code

--- MattoControlMain/ROS.h
+++ MattoControlMain/ROS.h
@@ -2,40 +2,66 @@
 #include "debug.h"
 #include "external_interupts.h"
 #include "sensors.h"
-#include "RGBS.h"
+#include "RGB_control.h"
+
+#ifndef ROS_H_
+#define ROS_H_
+
 
 ISR(INT2_vect) //interrupt 2 handler
 {
     EIMSK &= ~_BV(INT2); //Disable interrupt 2 (PB2)
     printf("\nin INT2 interrupt");
-    toggle_LEDs(1,0,0);
-    _delay_ms(33);
-    toggle_LEDs(0,1,0);
-    _delay_ms(33);
-    toggle_LEDs(0,0,1);
-    _delay_ms(33);
+    setRGBColour(1,0,0);
+    _delay_ms(333);
+    setRGBColour(0,1,0);
+    _delay_ms(333);
+    setRGBColour(0,0,1);
+    _delay_ms(333);
     int i = getWaterLevel();
-    switch (i)
-          case 100 : //white
-          case 90  : //blue
-          case 80  : //blue
-          case 70  : //green
-          case 60  : //green
-          case 50  : //yellow
-          case 40  : //yellow
-          case 30  : //amber
-          case 20  : //red
-          default  : //red
-          
+    
+    switch (i){
+          case 100 : //white 1,1,1
+            setRGBColour(1,1,1);
+            break;
+          case 90  : //cyan 0,1,1
+            setRGBColour(0,1,1);
+            break;
+          case 80  : //blue 0,0,1
+            setRGBColour(0,0,1);
+            break;
+          case 70  : //blue 0,0,1
+            setRGBColour(0,0,1);
+            break;
+          case 60  : //green 0,1,0
+            setRGBColour(0,1,0);
+            break;
+          case 50  : //green 0,1,0
+            setRGBColour(0,1,0);
+            break;
+          case 40  : //yellow 1,1,0
+            setRGBColour(1,1,0);
+            break;
+          case 30  : //yellow 1,1,0
+            setRGBColour(1,1,0);
+            break;
+          case 20  : //red 1,0,0
+            setRGBColour(1,0,0);
+            break;
+          default  : //red 1,0,0
+            setRGBColour(1,0,0);
+            break;
+    }
                 
    
     EIMSK |= _BV(INT2); //ReEnable interrupt 2 (PB2)
-    if (PINC&&_BV(2)) {
-    toggle_LEDs(0,0,1);
-    _delay_ms(33);
-    toggle_LEDs(0,1,0);
-    _delay_ms(33);
-    toggle_LEDs(1,0,0);
-    _delay_ms(33);
+    if (PINB&&_BV(2)) {
+    setRGBColour(0,0,1);
+    _delay_ms(333);
+    setRGBColour(0,1,0);
+    _delay_ms(333);
+    setRGBColour(1,0,0);
+    _delay_ms(333);
     }
 }
+#endif
